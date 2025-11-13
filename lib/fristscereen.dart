84da7ce69b47591b9233b34e2c440e061f0c6f41
Md. Al Assadulla <assad.it2003@gmail.com>
2025-11-14 01:02:45 +0600

    file creat b4 c6

---
+++ lib/fristscereen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Fasts extends StatefulWidget {
+  const Fasts({super.key});
+
+  @override
+  State<Fasts> createState() => _FastsState();
+}
+
+class _FastsState extends State<Fasts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:
+
+        Colors.red
+
+
+
+        ,
+      ),
+    );
+  }
+}
